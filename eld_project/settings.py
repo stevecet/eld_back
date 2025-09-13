8945fce43ea31d0af4e4d5fcab7eb18a0bc6ef73
stevecet <steveceto@outlook.fr>
2025-09-13 05:19:17 +0100

    Ensure eld_app initial migration is committed

--- eld_project/settings.py
+++ eld_project/settings.py
@@ -51,9 +51,17 @@ TEMPLATES = [
 ]
 
 # ✅ Use Postgres from Render (DATABASE_URL)
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ✅ Static files
 STATIC_URL = '/static/'
